c47dcfe584c7bdaca468f10281f95409c6c383f6
KubaGlegola <kubaglegola@gmail.com>
2022-11-14 18:33:02 +0100

    add form and filter section

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,10 +1,13 @@
 import "./Expenses.css";
 import ExpenseItem from "./ExpenseItem";
-import Card from "./Card";
+import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
 
 function Expenses(props) {
   return (
     <Card className="expenses">
+      <ExpensesFilter />
+
       {props.data.map((el) => (
         <ExpenseItem title={el.title} date={el.date} amount={el.amount} />
       ))}
